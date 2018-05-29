8d4b918ca0416e7e5f95e524976206d7b095d971
Your Name <you@example.com>
2018-05-29 22:53:14 +0700

    react導入。npmスクリップト追加。

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,15 @@ module.exports = {
   entry: './src/app.js',
   output: {
     filename: './dist/app.bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        // include: APP_DIR,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
   }
 }
