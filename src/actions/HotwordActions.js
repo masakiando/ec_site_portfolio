82563ef3f9a75cc9008e2e1a9f149f8cd4faa60c
Your Name <you@example.com>
2018-07-01 19:13:00 +0700

    debugger;　とる

--- src/actions/HotwordActions.js
+++ src/actions/HotwordActions.js
@@ -2,12 +2,10 @@ import * as types from './actionTypes';
 import hotwordApi from '../api/mockHotwordApi';
 
 export function loadHotwordsSuccess(hotwords) {
-  debugger;
   return { type: types.LOAD_HOTWORDS_SUCCESS, hotwords};
 }
 
 export function loadHotwords() {
-  debugger;
   return function(dispatch) {
     return hotwordApi.getAllHotwords()
     .then(hotwords => {
