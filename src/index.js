2376f86e56fbfa09bcd7aec1feedffbbf4beff66
Your Name <you@example.com>
2018-07-03 20:55:56 +0700

    redux導入

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import configureStore from './store/configureStore';
 import {loadHotwords} from './actions/HotwordActions';
+import {Provider} from 'react-redux';
 
 import './styles/styles.scss';
 import App from './components/App';
@@ -11,6 +12,8 @@ const store = configureStore();
 store.dispatch(loadHotwords());
 
 render(
-    <App/>,
+    <Provider store={store}>
+      <App/>
+    </Provider>,
     document.getElementById('root')
 );
