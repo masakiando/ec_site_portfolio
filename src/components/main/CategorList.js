09e7e7547bae92cf1ed77e738c13ac7e211ab4b8
Your Name <you@example.com>
2018-07-07 06:25:26 +0700

    スタイル調整

--- src/components/main/CategorList.js
+++ src/components/main/CategorList.js
@@ -11,15 +11,17 @@ const CategorList  = ({
             key={categor.id}>
            <a href="" className="categor__card">
              <div className="categor__container">
+               <div className="categor_img__container">
                <div className="categor_img"
                   style={
                     {backgroundImage: `url("./images/categories/${categor.cover}")`},
                      categorImgStyle
                   }
                     />
-                  <div className="categor__card-label">
-                 {categor.label}
-              </div>
+               </div>
+               <div className="categor__card-label">
+                {categor.label}
+               </div>
              </div>
            </a>
         </li>
@@ -35,6 +37,6 @@ CategorList.propTypes = {
 export default CategorList;
 
 let categorImgStyle = {
-  height:80,
-  width: 110
+  height:85,
+  width: 75
 };
