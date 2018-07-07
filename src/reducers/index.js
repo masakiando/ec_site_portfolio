05384f736b796df01cea8cb644e5cf0dda965c14
Your Name <you@example.com>
2018-07-07 20:44:03 +0800

    productReducer を　rootReducer に　ついか。

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import hotwords from './hotwordReducer';
 import categories from './CategorReducer';
+import products from './productReducer';
 
 const rootReducer = combineReducers({
   hotwords,
-  categories
+  categories: categories,
+  products: products
 });
 
 export default rootReducer;
